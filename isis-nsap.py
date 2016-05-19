5095144b470f38d8678e079b588d02b4e15da1f2
decolnz <aon@akkadian.co.tz>
2016-05-19 12:44:47 +0300

    Add files via upload

---
+++ isis-nsap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+ipAddress = sys.argv[1]
+
+octets = ipAddress.split('.', 4)
+
+netstring = '49.0001.'
+ipString = ''
+for octet in octets:
+    ipString += octet.zfill(3)
+
+n = 4
+for i in range(0, len(ipString), n):
+    netstring += ipString[i:i+n]
+    netstring += '.'
+
+netstring += '00'
+
+print(netstring)
